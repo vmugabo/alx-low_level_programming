61b70aa9130ba1894d3cb501f3a5e4db757075a3
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-08 14:41:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Print and that piece of art is useful" - Dora, Korpar, 2015-10-19
  * Return: 1 and not zero
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	putchar( "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
 	return (1);
 }
