30407a8284fde843208b5c0373a2f93193586972
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-08 13:15:55 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /**
  * main - We are printing a string without using printf
+ * return: return 0 else return non zero
  */
 int main(void)
 {
-/**
- * print the given string
- */
+/* print the given string*/
 	puts("\"Programming is like building a multilingual puzzle");
 /**
  *
  * return: Return 0 for correct output else return non zero
  */
- 	return (0);
+	return (0);
 }
 
