711526de086ab26d2bcef9145c07761726d35637
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-08 13:11:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,14 @@
  */
 int main(void)
 {
+/**
+ * print the given string
+ */
 	puts("\"Programming is like building a multilingual puzzle");
 /**
- * 
+ *
  * return: Return 0 for correct output else return non zero
  */
-
- return (0);
+ 	return (0);
 }
 
