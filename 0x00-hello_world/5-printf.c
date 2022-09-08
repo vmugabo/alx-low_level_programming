3a528820a773d904c3fae30bd8e7a45bfc82e4e4
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-08 13:29:57 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print "Programming is like building a multingual puzzle
+ * Return: 0 for proper execution else return non zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
