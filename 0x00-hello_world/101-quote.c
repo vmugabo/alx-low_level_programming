9d2de68d0f97518c61a423456f8466926f26383c
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-08 14:30:11 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print and that piece of art is useful" - Dora, Korpar, 2015-10-19
+ * Return: 1 and not zero
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	return (1);
+}
