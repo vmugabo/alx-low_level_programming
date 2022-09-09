0b06954e1e16d006b3f36e42170b014324c833fc
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-09 12:56:38 -0700

    if statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,19 +4,19 @@
 
 /**
  * main - random number assigned to n
- * Return: 0 for proper execution else non zero 
+ * Return: 0 for proper execution else non zero
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n  = rand() - RAND_MAX /2;
-	if (n >0)
+	n  = rand() - RAND_MAX / 2;
+	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n <0)
-		printf("%d is negative\n",n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	return (0);
 }
