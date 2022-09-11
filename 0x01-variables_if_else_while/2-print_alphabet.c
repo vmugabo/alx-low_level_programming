b7b77432cd4690e33e0ba68c855f5621e8d5edd1
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-11 07:51:05 -0700

    palphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-        char ch;
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
