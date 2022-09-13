a826549c96dd789685f61e9e83667c4af7bc533f
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-13 15:24:16 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * Main - print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * Return: Return 0 for success else return non zero
+*/
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
