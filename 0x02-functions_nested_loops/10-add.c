ada1618d61d22e48c845528727b8bace59fe7ec1
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-13 16:32:15 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: add to b
+ * @b: add to a
+ * Return:V  sum of a and b
+ */
+int add(int a, int b)
+{
+	int V;
+
+	V = a + b;
+
+	return (V);
+}
