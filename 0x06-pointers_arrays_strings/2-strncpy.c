ada521b1d49ecf54c7ff15a65cb3eb64a89a2771
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-22 06:42:46 -0700

    Corrected Strncpy fro betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
-* _strncat - Concatenates two strings
+* _strncpy - Concatenates two strings
 * @dest: destination string to
 * @src: sorce string from
 * @n:No. of strings
+* return: return destination
 *
 */
 
