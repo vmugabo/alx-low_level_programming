4b0cba11fa56f9c0d5df294fd4790feaf74636b2
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-09-22 08:51:19 -0700

    Print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main"
+
+/**
+ * print_number - prints integer;
+ * @n: integer printed
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
+}
