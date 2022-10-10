e266412e6445c5d653421a977871b434760f4e23
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-10-10 15:22:21 -0700

    Added dog.h for Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
 struct dog
 {
 	char *name;
