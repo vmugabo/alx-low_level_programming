86aedf760ed0551083a56c258b1db883a752cd5c
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-12-08 09:14:08 -0800

    Palindrome

---
+++ 0x17-doubly_linked_lists/102_palindrome.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int is_palindrome(int n)
+{
+    int m = n;
+
+    int rev = 0;
+    while (n > 0)
+    {
+        int d = n % 10;
+        rev = rev * 10 + d;
+        n /= 10;
+    }
+
+    return m == rev;
+}
+
+int main()
+{
+    int max_palindrome = 0;
+
+    for (int i = 100; i <= 999; i++)
+    {
+        for (int j = 100; j <= 999; j++)
+        {
+            int product = i * j;
+
+            if (is_palindrome(product) && product > max_palindrome)
+            {
+                max_palindrome = product;
+            }
+        }
+    }
+
+    printf("The largest palindrome made from the product of two 3-digit numbers is %d\n", max_palindrome);
+
+    return 0;
+}
